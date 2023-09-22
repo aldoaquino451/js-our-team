0456550e8bd8250140d8936d25acf0e89e48f393
aldoaquino451 <138690750+aldoaquino451@users.noreply.github.com>
2023-09-22 11:32:09 +0200

    [edit] template literal per tutto il div card

--- js/script.js
+++ js/script.js
@@ -11,45 +11,41 @@
 
 
 // 1. Dobbiamo creare un array che rappresenta l'intero team [array]
+// 2. Il team sarà composto da diversi oggetti che rappresentano i membri {object}
+// 3. Ogni membro sarà un oggetto con determinate proprietà come nome ruolo e foto { key : value, }
+
 const team = [
 
-  // 2. Il team sarà composto da diversi oggetti che rappresentano i membri {object}
   {
-    // 3. Ogni membro sarà un oggetto con determinate proprietà come nome ruolo e foto { key : value, }
     firstname : 'Wayne',
     lastname : 'Barnett',
-    role : 'Founder & CEO',
+    role : 'Founder & CEO', 
     profilePic : 'wayne-barnett-founder-ceo.jpg'
   },
-
   {
     firstname : 'Angela',
     lastname : 'Caroll',
     role : 'Chief Editor',
     profilePic : 'angela-caroll-chief-editor.jpg'
   },
-
   {
     firstname : 'Walter',
     lastname : 'Gordon',
     role : 'Office Manager',
     profilePic : 'walter-gordon-office-manager.jpg'
   },
-
   {
     firstname : 'Angela',
     lastname : 'Lopez',
     role : 'Social Media Manager',
     profilePic : 'angela-lopez-social-media-manager.jpg'
   },
-
   {
     firstname : 'Scott',
     lastname : 'Estrada',
     role : 'Developer',
     profilePic : 'scott-estrada-developer.jpg'
   },
-
   {
     firstname : 'Barbara',
     lastname : 'Ramos',
@@ -60,12 +56,39 @@ const team = [
 ]
 
 
+// 6. BONUS: Invece della lista inserisci i valori di tutte le proprietà all'interno di una card bootstrap
+
+for ( let member of team ) {
+
+  const firstname = member.firstname;
+  const lastname = member.lastname;
+  const role = member.role;
+  const profilePic = member.profilePic;
+
+  const container = document.getElementById('a-container');
+  
+  container.innerHTML += `
+  <div class="card a-card">
+    <img class="card-img-top" src="./img/${profilePic}" alt="${firstname} ${lastname}">
+    <div class="card-body">
+      <h3>${lastname} ${firstname}</h3>
+      <p class="card-text">${role}</p>
+    </div>
+  </div>
+  `;
+  
+};
+
+
+
+
+
+
 // 4. con un ciclo for in stampiamo in console tutte le proprietà coi relativi valori per ogni membro del team
+/*
+for ( let member of team ) {
 
-for ( let i in team ) {
-
-  const memberID = parseInt(i) + 1;
-  const member = team[i];
+  const memberID = parseInt(member) + 1;
 
   console.log('----- MEMBER NUMBER ' + memberID + ' -----');
   console.log(member.firstname);
@@ -75,6 +98,10 @@ for ( let i in team ) {
 
 };
 
+console.log('-------------***---------------')
+console.log(team)
+console.log(team[0])
+*/
 
 // 5. Lo stesso di sopra ma nell'html creiamo una lista dove stampare e visualizzare i dati
 /*
@@ -95,28 +122,3 @@ for ( let i in team ) {
 
 };
 */
-
-// 6. BONUS: Invece della lista inserisci i valori di tutte le proprietà all'interno di una card bootstrap
-
-for ( let i in team ) {
-  
-  const member = team[i];
-
-  const container = document.getElementById('a-container');
-  const card = document.createElement('div');
-  card.classList.add('card', 'a-card')
-
-  container.append(card);
-
-  card.innerHTML += `
-  <img class="card-img-top" src="./img/${member.profilePic}" alt="${member.firstname} ${member.lastname}">
-  `;
-
-  card.innerHTML += `
-  <div class="card-body">
-    <h3>${member.lastname} ${member.firstname}</h3>
-    <p class="card-text">${member.role}</p>
-  </div>
-  `;
-
-};
